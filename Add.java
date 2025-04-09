74bd815d65fadb1d63eafdbe919848fb57e6356e
sindhu_byali64 <sindhutbyali6@gmail.com>
2025-04-09 14:32:38 +0530

    saved in master

--- Add.java
+++ Add.java
@@ -4,7 +4,9 @@ public class Add {
         public static void main(String[] args) { // "static" should be lowercase
             int a = 10;
             int b = 20;
-            int sum = a + b;
-            System.out.println("Addition of a & b is: " + sum);
+            int sum= a + b;
+            int sub= a - b;
+            System.out.println("sum of a & b is: " + sum);
+            System.out.println("subtraction of a & b is: " + sub);
         }
 }
